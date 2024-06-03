cbd3e7d74e265e4594171c9e97a3c425d5b03bd9
Thomas <thomasribalta@gmail.com>
2024-06-03 17:52:58 +0200

    add time gestion to the shop

--- HTML-PHP Exercices/Little shop php/index.php
+++ HTML-PHP Exercices/Little shop php/index.php
@@ -1,4 +1,5 @@
 <?php 
+date_default_timezone_set("Europe/Paris");
 $title = "Little Shop";
 $euro = 0;
 $articles = [
@@ -33,11 +34,72 @@ $articles = [
       "image" => "image_shop/cerise.jpeg"
     ]
 ];
+define("JOURS", [
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+    "Dimanche"
+]);
+define("CRENAUX", [
+
+  [
+    [8, 12],
+    [14, 17]
+  ],
+  [
+    [8, 12],
+    [14, 19]
+  ],
+  [
+    [8, 12],
+    [14, 16]
+  ],
+  [
+    [8, 12],
+    [14, 21]
+  ],
+  [
+    [8, 12],
+    [14, 19]
+  ],
+  [
+    [8, 12],
+    [14, 23]
+  ],
+  [
+    [8, 12],
+    [14, 23]
+  ]
+]);
+
+function is_open (): bool
+{
+  $open = false;
+  if (((int)date("H") >= (int) CRENAUX[(int)date("N") - 1][0][0] && (int) date("H") <= (int) CRENAUX[(int)date("N") - 1][0][1]) || 
+    ((int)date("H") >= (int) CRENAUX[(int)date("N") - 1][1][0] && (int) date("H") <= (int) CRENAUX[(int)date("N") - 1][1][1])) {
+    $open = true;
+  }
+  return $open;
+}
+
 require "header.php";
 ?>
 
-<?php if (isset($_POST["article"])): ?>
-    <div class="alert alert-success mt-5" role="alert">
+<?php if (is_open()): ?>
+    <div class="alert alert-success mt-1" role="alert">
+      L'e-boutique est ouverte !
+    </div>
+<?php else: ?>
+    <div class="alert alert-danger mt-1" role="alert">
+      L'e-boutique est fermée !
+    </div>
+<?php endif; ?>
+
+<?php if (isset($_POST["article"]) && is_open()): ?>
+    <div class="alert alert-success mt-1" role="alert">
       Vous avez acheté :
         <?php foreach ($_POST["article"] as $value): ?>
             <?=$value?>
@@ -64,3 +126,29 @@ require "header.php";
         <input type="submit" class="btn btn-primary mt-5" value="Acheter ces articles">
     </div>
 </form>
+
+<div class="row mt-5">
+  <div class="col">
+  </div>
+  <div class="col text-center">
+    <h2>Horaires d'ouvertures de l'e-boutique</h2>
+    <table class="table table-striped table-bordered mt-3">
+      <thead>
+        <tr>
+          <?php for ($i = 0; $i < count(JOURS); $i++): ?>
+            <th scope="col" <?php if ($i + 1 === (int) date("N")): ?> class="bg-success" <?php endif;?>><?=JOURS[$i]?></th>
+          <?php endfor; ?>
+        </tr>
+      </thead>
+      <tbody>
+        <?php for ($i = 0; $i < 2; $i++): ?>
+          <tr>
+            <?php for ($j = 0; $j < count(JOURS); $j++): ?>
+              <td><?=CRENAUX[$j][$i][0]?>h - <?=CRENAUX[$j][$i][1]?>h</td>
+            <?php endfor; ?>
+          </tr>
+        <?php endfor; ?>
+      </tbody>
+    </table>
+  </div>
+</div>
